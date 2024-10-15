2dc4ee45dfaf793cf5b8ab4fd3c9fe4862995dda
Mukhtar Muhammed Yahaya <126781903+mmyahaya@users.noreply.github.com>
2024-10-15 13:31:40 +0200

    add description

--- R/taxa_cube.R
+++ R/taxa_cube.R
@@ -1,3 +1,27 @@
+#' @title Prepare Data Cubes
+#'
+#'@description Prepare data cube to calculate species impact . The function `taxa_cube`
+#' can take in the scientific name of the taxa of interest as in character or 
+#' GBIF occurrences data containing necessary columns. The GBIF occurrences is 
+#' downloaded if scientific names is given. The function returns data cubes of 
+#' focal taxa and reference taxa. If the reference taxa is not specified, the 
+#' function returns the focal taxa as reference taxa.
+#'@param taxa Character or dataframe. The character should be the scientific 
+#'name of the focal taxa while the dataframe is the GBIF occurrences data which must 
+#'contain "decimalLatitude","decimalLongitude","species","speciesKey",
+#'"coordinateUncertaintyInMeters","dateIdentified", and "year".
+#'@param country.sf sf object. The shapefile of the region of study
+#'@param limit Integer. Number of records to return from GBIF download.
+#'Default is set to 500
+
+#' @param country Character. Country for which the GBIP occurrences data should 
+#' be downloaded. Country should be 2-letter country code (ISO-3166-1).
+#' Default is ZA
+#' @param res Numeric. The resolution of grid cells to be used. Default is 0.25
+#'
+#' @return A dataframe containing the `sim_cubes` of taxa.
+#' 
+
 taxaFun <- function(taxa,country.sf,limit=500,country='ZA',res=0.25){
   
   grid <- country.sf %>%
@@ -7,7 +31,7 @@ taxaFun <- function(taxa,country.sf,limit=500,country='ZA',res=0.25){
     sf::st_sf() %>%
     dplyr::mutate(cellid = dplyr::row_number())
   
-  # download taxa (target taxa) if the scientific name is given as character
+  # download taxaif the scientific name is given as character
   if("character" %in% class(taxa)){
     taxa.gbif_download = rgbif::occ_data(scientificName=taxa, # download data from gbif
                                          country=country,
@@ -18,8 +42,10 @@ taxaFun <- function(taxa,country.sf,limit=500,country='ZA',res=0.25){
     taxa.df = as.data.frame(taxa.gbif_download$data) #extract data from the downloaded file
   } else if("data.frame" %in% class(taxa)){ #check if data fame contains the required columns
     if(any(!c("decimalLatitude","decimalLongitude",
-              "species","speciesKey","coordinateUncertaintyInMeters","dateIdentified","year") %in% colnames(taxa))){
-      requiredcol<-c("decimalLatitude","decimalLongitude","species","coordinateUncertaintyInMeters","dateIdentified","year")
+              "species","speciesKey","coordinateUncertaintyInMeters",
+              "dateIdentified","year") %in% colnames(taxa))){
+      requiredcol<-c("decimalLatitude","decimalLongitude","species",
+                     "speciesKey","iucnRedListCategory","coordinateUncertaintyInMeters","dateIdentified","year")
       missingcol<-requiredcol[!c("decimalLatitude","decimalLongitude","species","speciesKey","coordinateUncertaintyInMeters","dateIdentified","year") %in% colnames(taxa)]
       cli::cli_abort(c("{missingcol} is/are not in the {.var taxa} column ", 
                        "x" = "{.var taxa} should be a data of GBIF format "))
@@ -33,7 +59,7 @@ taxaFun <- function(taxa,country.sf,limit=500,country='ZA',res=0.25){
   
   taxa.sf = taxa.df %>%
     dplyr::select(decimalLatitude,decimalLongitude,
-                  species,speciesKey,coordinateUncertaintyInMeters,dateIdentified,year) %>% #select occurrence data
+                  species,speciesKey,iucnRedListCategory,coordinateUncertaintyInMeters,dateIdentified,year) %>% #select occurrence data
     dplyr::filter_all(all_vars(!is.na(.))) %>% # remove rows with missing data
     dplyr::filter(coordinateUncertaintyInMeters<=res*1000) %>% 
     #dplyr::mutate(coordinateUncertaintyInMeters = coordinateUncertaintyInMeters/(res*1000)^2) %>% 
@@ -59,3 +85,7 @@ taxaFun <- function(taxa,country.sf,limit=500,country='ZA',res=0.25){
   
   return(taxa_cube)
 }
+
+# countries_sf<-readRDS(paste0(getwd(),"/Data/countries_shapefile.rds"))
+# SA.sf<-filter(countries_sf,name=="South Africa") %>% select(name,geometry)
+# taxa_cube<-taxaFun(taxa = occurrence_fabacae, country.sf = SA.sf)
