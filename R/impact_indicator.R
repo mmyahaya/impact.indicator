5649944878ff416ba6d479012454ad6bed9bfcff
Yahaya <26485613@sun.ac.za>
2024-10-18 18:33:31 +0200

    create impact values

--- R/impact_indicator.R
+++ R/impact_indicator.R
@@ -1,66 +1,72 @@
-
+# install.packages("devtools")
+devtools::install_github("b-cubed-eu/b3gbi")
 
 
 sbs.fun<-function(y){
-  sbs.taxon<-taxon_cube$data %>%
-    filter(year==y) %>% 
+  sbs.taxon<-taxa_cube$data %>%
+    dplyr::filter(year==y) %>%
     dplyr::select(scientificName,cellCode,obs) %>%
-    group_by(scientificName,cellCode) %>%
-    summarise(across(obs, sum), .groups = "drop") %>%
-    pivot_wider(names_from = scientificName, values_from = obs) %>%
-    arrange(cellCode) %>% 
-    column_to_rownames(var = "cellCode") 
+    dplyr::group_by(scientificName,cellCode) %>%
+    dplyr::summarise(across(obs, sum), .groups = "drop") %>%
+    tidyr::pivot_wider(names_from = scientificName, values_from = obs) %>%
+    dplyr::arrange(cellCode) %>%
+    tibble::column_to_rownames(var = "cellCode")
   return(sbs.taxon)
 }
 
-full_species_list<-sort(unique(taxon_cube$data$scientificName))
-period=
+full_species_list<-sort(unique(taxa_cube$data$scientificName))
+period=unique(taxa_cube$data$year)
 
 sbs.taxon_list<-map(period,sbs.fun)
-for(y in period[-c(1:12)]){
-  
-  
-  sbs.taxon_list[[paste0("year","_",y)]]<-sbs.taxon
-  
+
+impact_values<-c()
+for(y in period){
+
+
+  #sbs.taxon_list[[paste0("year","_",y)]]<-sbs.taxon
+  sbs.taxon<-sbs.fun(y)
+
   species_list<-unique(names(sbs.taxon))
-  
-  if(!exists("taxon_status_list")){
-    full_species_list<-sort(unique(taxon_cube$data$scientificName))
-    taxon_status_list<-taxon_status(species_list = full_species_list,
+
+  if(!exists("taxa_status_list")){
+    full_species_list<-sort(unique(taxa_cube$data$scientificName))
+    taxa_status_list<-taxa_status(species_list = full_species_list,
                                     source = "WCVP",
                                     region = "South Africa")
   }
-  
-  intro.sf<-taxon_cube$data %>%
-    filter(year==y) %>%
-    left_join(taxa_list_status,
+
+  intro.sf<-taxa_cube$data %>%
+    dplyr::filter(year==y) %>%
+    dplyr::left_join(taxa_status_list,
               by = c("scientificName" = "taxon"))
-  
-  
+
+
   status.sf <- intro.sf %>%
-    group_by(cellCode) %>%
-    summarise(
+    dplyr::group_by(cellCode) %>%
+    dplyr::summarise(
       total_intro_obs = sum(obs[introduction_status == "introduced"], na.rm = TRUE),
       total_native_obs = sum(obs[introduction_status == "native"], na.rm = TRUE),
       .groups = "drop"
     ) %>%
-    mutate(across(c(total_intro_obs, total_native_obs), ~ ifelse(.==0,NA,.))) %>%
-    mutate(intro_native=total_intro_obs/total_native_obs) %>%
-    arrange(cellCode)
+    dplyr::mutate(dplyr::across(c(total_intro_obs, total_native_obs), ~ ifelse(.==0,NA,.))) %>%
+    dplyr::mutate(intro_native=total_intro_obs/total_native_obs) %>%
+    dplyr::arrange(cellCode)
   if (!exists("eicat_score_list")){
-    eicat_score_list=eicat_impact(eicat_data = eicat_data,species_list = species_list,
+    eicat_score_list=impact_cat(data = eicat_data,species_list = species_list,
                                   fun="max")
   }
-  
+
   eicat_score<-eicat_score_list[species_list,]
-  
+
   siteScore<-status.sf$intro_native
-  
-  impact_metrics<-list(year=y,sbs.taxon=sbs.taxon,eicat_score=eicat_score,siteScore)
-  impact_list[[as.character(y)]]<-impact_metrics
+
+  abdundance_impact = sweep(sbs.taxon,2,eicat_score,FUN = "*")
+  impactScore = siteScore*abdundance_impact
+  impact<-sum(impactScore,na.rm = TRUE)
+  impact_values<-c(impact_values,impact)
 }
 
-
+impact_values<-c()
 
 
 species_list<-unique(names(sbs.taxon))
@@ -80,8 +86,8 @@ taxon_status_list<-taxon_status(species_list = species_list,
 
 
 
-intro.sf<-taxon_cube$data %>% 
-  filter(year==y) %>% 
+intro.sf<-taxa_cube$data %>%
+  filter(year==y) %>%
   left_join(taxa_list_status,
             by = c("scientificName" = "taxon"))
 
@@ -92,9 +98,9 @@ status.sf <- intro.sf %>%
     total_intro_obs = sum(obs[introduction_status == "introduced"], na.rm = TRUE),
     total_native_obs = sum(obs[introduction_status == "native"], na.rm = TRUE),
     .groups = "drop"
-  ) %>% 
-  mutate(across(c(total_intro_obs, total_native_obs), ~ ifelse(.==0,NA,.))) %>% 
-  mutate(intro_native=total_intro_obs/total_native_obs) %>% 
+  ) %>%
+  mutate(across(c(total_intro_obs, total_native_obs), ~ ifelse(.==0,NA,.))) %>%
+  mutate(intro_native=total_intro_obs/total_native_obs) %>%
   arrange(cellCode)
 
 
@@ -114,7 +120,7 @@ impact<-sum(impactScore,na.rm = TRUE)
 
 # specieImpact<-colSums(impactScore,na.rm = TRUE)
 # specieImpact<-specieImpact[specieImpact>0]
-# 
+#
 # siteImpact<-rowSums(impactScore,na.rm = TRUE)
 # siteImpact<-siteImpact[siteImpact>0]
 
